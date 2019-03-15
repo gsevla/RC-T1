1ce2b8ccd6a361880507ce94b0f39283eb251886
gsevla <36795971+gsevla@users.noreply.github.com>
2019-03-14 23:14:03 -0300

    versão final

--- proxy.py
+++ proxy.py
@@ -11,69 +11,64 @@ bufferSize = 4096
 def proxyServer(webserver, port, connection, addr, message):
     try:
         tcpSocket = socket(AF_INET, SOCK_STREAM)
-        print('entrou')
-        print(webserver)
-        print(port)
+        tcpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)    # Use the address port even it is blocked(root requires)
         tcpSocket.connect((webserver, port))
-        print('conectou')
-        print(message)
+        print("proxy <==> client connection started.")
         tcpSocket.send(message)
-        print('socket criado ok')
+
         while 1:
             answer = tcpSocket.recv(bufferSize)
 
             if(len(answer) > 0):
                 connection.send(answer)
-                print('tudo alright')
+                print("Answer sent!")
             else:
                 break
+            
         tcpSocket.close()
         connection.close()
+        print("proxy <==> client connection ended.")
     except Exception:
-        print('huehue')
+        print()
+        print("Something do wrong while sending message from proxy to browser!")
+        print("The port must be blocked, try to run as root if you aren't.\n")
 
 def connString(connection, message, addr):
     try:
+        print("####\nTo connection: {}".format(addr))
         url = str(message).split('\n')[0].split(' ')[1]
-        print('####################')
-        print(url)
-        noHttpUrl = url.find("://") # prevents http/https/whatever
-        print(noHttpUrl)
-        print('####################')
+        print("## url: {}".format(url))
+        noHttpUrl = url.find("://") # prevents http/https/whatever, but no duplicated urls/strange urls
+
         if(noHttpUrl == -1):
             temp = url
-            print('a')
-            print(temp)
         else:
             temp = url[(noHttpUrl+3):]
-            print('b')
-            print(temp)
         
         portPos = temp.find(":")
         webserverPos = temp.find("/")
 
         if(webserverPos == -1):
             webserverPos = len(temp)
-            print(webserverPos)
-            print('c')
+
         webserver = ''
         port = -1
-        print('d')
+
         if(portPos == -1 or webserverPos < portPos):
             port = 80
             webserver = temp[:webserverPos]
-            print('e')
         else:
             port = int((temp[(portPos+1):])[:webserverPos-portPos-1])
             webserver = temp[:portPos]
-            print('f')
-        print('g')
-        print(webserver)
+
+        print("## webserver: {}".format(webserver))
+        print("####\n")
         
         proxyServer(webserver, port, connection, addr, message)
         
     except Exception:
-        print('hue')
+        print()
+        print("Something do wrong in message manipulation!\n")
 
 
 
@@ -85,20 +80,23 @@ def main():
         tcpSerSock.bind((sys.argv[1], 8888))
         tcpSerSock.listen(mutualConnections)
     except Exception: 
+        print()
         print("Can't start server!")
+        print("The port must be blocked, try to run as root if you aren't.\n")
         sys.exit(2)
 
     while 1:
         try:
             # Strat receiving data from the client
-            print('Ready to serve...')
+            print('Ready to serve...\n')
             connection, addr = tcpSerSock.accept()
             print('Received a connection from:', addr)
             message = connection.recv(bufferSize)
-            #print(message)
+            print("Starting a new thread...\n")
             threading._start_new_thread(connString, (connection, message, addr))
         except Exception:
-            print("Something's wrong! Closing server in 1...")
+            print()
+            print("Something's wrong! Closing server in 1...\n")
             tcpSerSock.close()
             sys.exit(1)
     
@@ -112,8 +110,11 @@ if __name__ == "__main__":
         try:
             main()
         except KeyboardInterrupt: 
-            print("\nUser interruption!")
+            print()
+            print("\nUser interruption!zn")
             sys.exit(2)
     else:
-        print('Usage : "python3 proxy.py server_ip"\nserver_ip : It is the IP Address Of Proxy Server')
+        print()
+        print('Usage : "python3 proxy.py server_ip"')
+        print("server_ip : It is the IP Address Of Proxy Server\n")
         sys.exit(2)
